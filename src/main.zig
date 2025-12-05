4e96ee442c22bda0ab97c420ead60d59b2b28130
Mehmet Denizhan Erdem <denizhanerdem@pm.me>
2025-12-05 20:37:18 +0000

    cxc

--- src/main.zig
+++ src/main.zig
@@ -6,5 +6,9 @@ const day03 = @import("./day03.zig").day03;
 const day04 = @import("./day04.zig").day04;
 const day05 = @import("./day05.zig").day05;
 pub fn main() !void {
+    // day01();
+    // day02();
+    // day03();
+    // day04();
     day05();
 }
